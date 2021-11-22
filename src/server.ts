33ad4471419f75ea1dc5e0c7f349c5544171fb86
RafaelSantinon <rafaelns94@outlook.com>
2021-11-22 00:21:03 -0300

    feat: create .env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import { Constants } from './utils/constants';
 
 // eslint-disable-next-line
 import './database';
@@ -8,7 +9,7 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ message: 'Hello world!' }));
 
-app.listen(3333, () => {
+app.listen(Constants.port, () => {
   /* eslint-disable no-console */
-  console.log('Server is running!!');
+  console.log(`Server is running - PORT: ${Constants.port}!!!`);
 });
